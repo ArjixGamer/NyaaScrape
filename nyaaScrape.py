251e95c80c870d26607521b07288f9489291fb80
ArjixGamer <53124886+ArjixGamer@users.noreply.github.com>
2020-04-18 20:37:06 +0300

    Update nyaaScrape.py

--- nyaaScrape.py
+++ nyaaScrape.py
@@ -5,113 +5,112 @@ import re
 import os
 import subprocess
 import sys
+import click
+@click.command()
+@click.option('--type', '-t', help='Choose Anime or Manga.                            Default: Anime.', default='Anime', metavar='Anime/Manga')
+@click.option('--search', '-s', help='Search', required=True, type=str, metavar='Anime/Manga')
+@click.option('--filter', '-f', help='Filter by: size(0), seeders(1), id(2).          Default: Size.', default=0)
+@click.option('--sort', help='Sort by: ascending(1), descending(0).          Default: Descending.', default=0)
+def main(type, search, filter, sort):
+    def userSelection():
+        cataChecker = type
+        if cataChecker == 'Anime' or cataChecker == 'anime' or cataChecker == 'a':
+            filt = filterCheck()
+            ud = udCheck()
+            link = f'https://nyaa.si/?q={search}&f=0&c=1_2&s={filt}&o={ud}'
+            return link
+        elif cataChecker == 'Manga' or cataChecker == 'manga' or cataChecker == 'm':
+            filt = filterCheck()
+            ud = udCheck()
+            link = f'https://nyaa.si/?q={search}&f=0&c=3_1&s={filt}&o={ud}'
+            return link
+        else:
+            raise Exception("Please insert anime or manga!")
+            
+    def filterCheck():
+        filta = ['size','seeders','id']
+        ud = ['asc','desc']
+        if filter < 3 and filter > -1:
+            return filta[filter]
+        else:
+            raise Exception("Please use the correct selections!")
+            
+    def udCheck():
+        ud = ['desc','asc']
+        sort
+        if sort < 2 and sort > -1:
+            return ud[sort]
+        else:
+            raise Exception("Please use the correct selections!")
 
-def userSelection():
-    cataChecker = sys.argv[1]
-    if cataChecker == 'Anime' or cataChecker == 'anime' or cataChecker == 'a':
-        search = showCheck()
-        filt = filterCheck()
-        ud = udCheck()
-        link = f'https://nyaa.si/?q={search}&f=0&c=1_2&s={filt}&o={ud}'
-        return link
-    elif cataChecker == 'Manga' or cataChecker == 'manga' or cataChecker == 'm':
-        search = showCheck()
-        filt = filterCheck()
-        ud = udCheck()
-        link = f'https://nyaa.si/?q={search}&f=0&c=3_1&s={filt}&o={ud}'
-        return link
-    else:
-        raise Exception("Please insert anime or manga!")
-        
-def filterCheck():
-    filta = ['size','seeders','id']
-    ud = ['asc','desc']
-    filt = 1
-    if int(filt) < 3 and int(filt) > -1:
-        return filta[int(filt)]
-    else:
-        raise Exception("Please use the correct selections!")
-        
-def udCheck():
-    ud = ['desc','asc']
-    choice = 0
-    if int(choice) < 2 and int(choice) > -1:
-        return ud[int(choice)]
-    else:
-        raise Exception("Please use the correct selections!")
+    def torrentDownloader(magnet,inOrout):
+        if inOrout == 'Y' or inOrout == 'y':
+            os.startfile(magnet)
+        else:
+            def run_command(magnet):
+                process = subprocess.Popen(f'aria2c {magnet}', stdout=subprocess.PIPE)
+                while True:
+                    output = process.stdout.readline()
+                    if output == '' and process.poll() is not None:
+                        break
+                    if output:
+                        print (output.strip())
+                rc = process.poll()
+                return rc
+            run_command(magnet)
+            
 
-def showCheck():
-    search = str(sys.argv[2])
-    return search
+    def searchResults():
+        Storage = [
+            SearchResult(
+            title = i.select("a:not(.comments)")[1].get("title"),
+            size = i.find_all('td',class_ = 'text-center')[1].text,
+            seeders = i.find_all('td',class_= 'text-center')[3].text,
+            leechers = i.find_all('td',class_ = 'text-center')[4].text,
+            magnet = i.find_all('a',{'href':re.compile(regex)})[0].get('href'))
+            for i in lol.select("tr.default, tr.success")
+        ]
+        return Storage
 
-def torrentDownloader(magnet,inOrout):
-    if inOrout == 'Y' or inOrout == 'y':
-        os.startfile(magnet)
-    else:
-        def run_command(magnet):
-            process = subprocess.Popen(f'aria2c {magnet}', stdout=subprocess.PIPE)
-            while True:
-                output = process.stdout.readline()
-                if output == '' and process.poll() is not None:
-                    break
-                if output:
-                    print (output.strip())
-            rc = process.poll()
-            return rc
-        run_command()
-        
+    class SearchResult:
+        def __init__(self, title, size, seeders, leechers, magnet, poster='', meta=''):
+            self.title = title
+            self.magnet = magnet
+            self.size = size
+            self.leechers = leechers
+            self.seeders = seeders
+            self.poster = poster
+            self.meta = meta
 
-def searchResults():
-    Storage = [
-        SearchResult(
-        title = i.select("a:not(.comments)")[1].get("title"),
-        size = i.find_all('td',class_ = 'text-center')[1].text,
-        seeders = i.find_all('td',class_= 'text-center')[3].text,
-        leechers = i.find_all('td',class_ = 'text-center')[4].text,
-        magnet = i.find_all('a',{'href':re.compile(regex)})[0].get('href'))
-        for i in lol.select("tr.default, tr.success")
-    ]
-    return Storage
+        def __repr__(self):
+            return '<SearchResult Title: {} Size: {} Seeders: {} Leechers: {} Magnet: {}>'.format(self.title, self.size,
+                                                                                                self.seeders,
+                                                                                                self.leechers,
+                                                                                                self.magnet)
+        def __str__(self):
+            return self.title
 
-class SearchResult:
-    def __init__(self, title, size, seeders, leechers, magnet, poster='', meta=''):
-        self.title = title
-        self.magnet = magnet
-        self.size = size
-        self.leechers = leechers
-        self.seeders = seeders
-        self.poster = poster
-        self.meta = meta
-
-    def __repr__(self):
-        return '<SearchResult Title: {} Size: {} Seeders: {} Leechers: {} Magnet: {}>'.format(self.title, self.size,
-                                                                                            self.seeders,
-                                                                                            self.leechers,
-                                                                                            self.magnet)
-    def __str__(self):
-        return self.title
-
-    @property
-    def pretty_metadata(self):
-        """
-        pretty_metadata is the prettified version of metadata
-        """
-        if self.meta:
-            return ' | '.join(val for _, val in self.meta.items())
-        return ''
-
+        @property
+        def pretty_metadata(self):
+            """
+            pretty_metadata is the prettified version of metadata
+            """
+            if self.meta:
+                return ' | '.join(val for _, val in self.meta.items())
+            return ''
+        regex = r'(magnet:)+[^"]*'
+        Link = userSelection()
+        page = requests.get(Link).text
+        lol = bs(page, 'html.parser')
+        Storage = searchResults()
+        for i in range(len(Storage)):
+            print(i, ' ', Storage[i], '\n',
+                  Storage[i].size, '\n',
+                  'Seeders', Storage[i].seeders )
+        choice = input('Which one would you like to select? \n')
+        inOrout = 'n'
+        magnet = Storage[choice].magnet
+        torrentDownloader(magnet,inOrout)
+        print('Download has completed.')
 if __name__ == '__main__':
-    regex = r'(magnet:)+[^"]*'
-    Link = userSelection()
-    page = requests.get(Link).text
-    lol = bs(page, 'html.parser')
-    Storage = searchResults()
-    for i in range(len(Storage)):
-        print(i, ' ', Storage[i], '\n',
-              Storage[i].size, '\n',
-              'Seeders', Storage[i].seeders )
-    choice = input('Which one would you like to select? \n')
-    inOrout = 'n'
-    magnet = Storage[int(choice)].magnet
-    torrentDownloader(magnet,inOrout)
-    print('Download has completed.')
+        main()
